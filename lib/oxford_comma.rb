25c1dd102c7c7c4f965c7819eb7514775c6b9a48
msenior526 <msenior526@gmail.com>
2020-10-30 13:15:38 -0400

    commit tree 726d07675d094db7e003bf5c3d80651cba535d2f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require "pry"
 def oxford_comma(array)
+if array.size == 1
+return  "#{array[0]}"
+elsif array.size == 2
+  return array.join(" and ")
+elsif array.size >= 3
+  new_array = array
+  new_array[-1] = "and #{new_array[-1]}"
+  new_array.join(", ")
+
+
+
+  end
 
 end
